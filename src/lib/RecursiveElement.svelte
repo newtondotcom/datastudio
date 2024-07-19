b4965d06b1fa433561218ca91026c9e21a9517fe
newtondotcom <asphalt9fr@gmail.com>
2024-07-19 21:18:20 +0200

    feat : add multiplicity, type and name input for component

--- src/lib/RecursiveElement.svelte
+++ src/lib/RecursiveElement.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
-	import type { Element } from '../ambient';
+	import { ChevronDown, ChevronUp } from 'lucide-svelte';
+	import type { Element } from '../ambient'; 
+	import { Input } from "$lib/components/ui/input/index.js";
+    import Delete from './components/studio/Delete.svelte';
+    import Type from './components/studio/Type.svelte';
+    import Multiplicity from './components/studio/Multiplicity.svelte';
+    import Rename from './components/studio/Rename.svelte';
 
 	export let children: Element[] = [];
 	export let indent = 0;
@@ -13,31 +19,48 @@
 	}
 </script>
 
-<div>
-	<button style="padding-left: {indent}px" on:click={toggleOpen} class="bg-gray-400">
+<div class="w-full px-4">
+	<div class="flex flex-row">
+	<button style="padding-left: {indent}px" 
+	on:click={toggleOpen} 
+	class="bg-gray-400 flex flex-row w-full text-3xl rounded-lg px-4 py-2">
 		{name}
-		{open ? '(opened)' : '(closed)'}
+		{#if open}
+			<ChevronDown size={40} />
+		{:else}
+			<ChevronUp size={40}/>
+		{/if}
 	</button>
+	<Rename />
+	<Delete />
+	</div>
 
 	{#if open}
 		<ul>
 			<div class="overflow-hidden transition-all duration-300 ease-in-out">
 				{#each children as child}
-					<li class="flex flex-row">
-						{#if child.type == 'struc'}
-							<div>{child.name}</div>
-						{:else}
-							<div>{child.name}</div>
-							<div>{child.type}</div>
-							<div>{child.multiplicity.with}</div>
-						{/if}
-					</li>
+				<li class="flex flex-row text-2xl py-2 px-2 border-2 rounded-lg mx-1 my-1">
+					{#if child.type == 'struc'}
+					<li>
 					<svelte:self
 						children={child.children}
-						indent={indent + 24}
+						indent={indent + 1}
 						{structure}
 						name={child.name}
-					/>
+						/>
+					</li>
+					{:else}
+
+					<Input 
+					value={child.name} 
+					type="email" 
+					placeholder="Name" 
+					class="max-w-xs" />
+					<Type />
+					<Multiplicity />
+					{/if}
+				</li>
+
 				{/each}
 			</div>
 		</ul>
