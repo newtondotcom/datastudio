46c182d982ff4413b3dabfb1c37498c4592cfd75
newtondotcom <asphalt9fr@gmail.com>
2024-07-19 21:49:05 +0200

    feat : define the abstraction of the type created

--- src/lib/components/studio/Type.svelte
+++ src/lib/components/studio/Type.svelte
@@ -9,27 +9,28 @@
 	import { cn } from '$lib/utils.js';
 	import { Label } from '$lib/components/ui/label/index.js';
 	import { tick } from 'svelte';
+	import { Checkbox } from '$lib/components/ui/checkbox';
 
 	const frameworks = [
 		{
-			value: 'sveltekit',
-			label: 'SvelteKit'
+			value: 'string',
+			label: 'String'
 		},
 		{
-			value: 'next.js',
-			label: 'Next.js'
+			value: 'integer',
+			label: 'Integer'
 		},
 		{
-			value: 'nuxt.js',
-			label: 'Nuxt.js'
+			value: 'float',
+			label: 'Float'
 		},
 		{
-			value: 'remix',
-			label: 'Remix'
+			value: 'boolean',
+			label: 'Boolean'
 		},
 		{
-			value: 'astro',
-			label: 'Astro'
+			value: 'data',
+			label: 'Date'
 		}
 	];
 
@@ -81,16 +82,45 @@
 				{/each}
 				<Sheet.Root>
 					<Sheet.Trigger>
-						<Command.Item>Create a type</Command.Item>
+						<Command.Item>
+							<Check class="mr-2 h-4 w-4 text-transparent" />
+							Create a type
+						</Command.Item>
 					</Sheet.Trigger>
 					<Sheet.Content>
 						<Sheet.Header>
-							<Sheet.Title>Are you sure absolutely sure?</Sheet.Title>
+							<Sheet.Title>Create a type</Sheet.Title>
 							<Sheet.Description>
 								This action cannot be undone. This will permanently delete your account and remove
 								your data from our servers.
 							</Sheet.Description>
 						</Sheet.Header>
+						<div class="grid gap-4 py-4">
+							<div class="grid grid-cols-4 items-center gap-4">
+								<Label for="name" class="text-right">Type Name</Label>
+								<Input id="name" value="Pedro Duarte" class="col-span-3" />
+							</div>
+							<div class="items-top flex space-x-2">
+								<Checkbox id="terms1" />
+								<div class="grid gap-1.5 leading-none">
+									<Label
+										for="terms1"
+										class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+									>
+										Is this not an abstract type ?
+									</Label>
+									<p class="text-sm text-muted-foreground">
+										If you select this option, the type will be created as a concrete type (ex :
+										String, Integer, Float, Boolean, Date)
+									</p>
+								</div>
+							</div>
+						</div>
+						<Sheet.Footer>
+							<Sheet.Close asChild let:builder>
+								<Button builders={[builder]} type="submit">Save changes</Button>
+							</Sheet.Close>
+						</Sheet.Footer>
 					</Sheet.Content>
 				</Sheet.Root>
 			</Command.Group>
