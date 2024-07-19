457a89f043d2204634ef25504ed06e52df7013c1
newtondotcom <asphalt9fr@gmail.com>
2024-07-19 23:02:34 +0200

    feat : new realistic data schema

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,80 +5,243 @@
 	const elements: Element[] = [
 		{
 			id: '1',
-			name: 'Element 1',
+			name: 'Person',
+			type: 'struc',
+			multiplicity: 1,
+			id_parent: null
+		},
+		{
+			id: '1.1',
+			name: 'Name',
+			type: 'struc',
+			multiplicity: 1,
+			id_parent: '1'
+		},
+		{
+			id: '1.1.1',
+			name: 'First Name',
 			type: 'string',
-			multiplicity: {
-				with: 'Structure',
-				number: 1
-			},
-			id_parent: null,
-			children: [
-				{
-					id: '1.1',
-					name: 'Element 1.1',
-					type: 'number',
-					multiplicity: {
-						with: 'Element 1',
-						number: 0
-					},
-					id_parent: '1',
-					children: []
-				},
-				{
-					id: '1.2',
-					name: 'Element 1.2',
-					type: 'struc',
-					multiplicity: {
-						with: 'Element 1',
-						number: 1
-					},
-					id_parent: '1',
-					children: [
-						{
-							id: '1.2.1',
-							name: 'Element 1.2.1',
-							type: 'date',
-							multiplicity: {
-								with: 'Element 1.2',
-								number: 1
-							},
-							id_parent: '1.2',
-							children: []
-						},
-						{
-							id: '1.2.4',
-							name: 'Element 1.2.4',
-							type: 'date',
-							multiplicity: {
-								with: 'Element 1.4',
-								number: 1
-							},
-							id_parent: '1.1',
-							children: []
-						}
-					]
-				},
-				{
-					id: '1.3',
-					name: 'Element 1.3',
-					type: 'string',
-					multiplicity: {
-						with: 'Element 1',
-						number: 1
-					},
-					id_parent: '1',
-					children: []
-				}
-			]
+			multiplicity: 1,
+			id_parent: '1.1'
+		},
+		{
+			id: '1.1.2',
+			name: 'Last Name',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.1'
+		},
+		{
+			id: '1.2',
+			name: 'Address',
+			type: 'struc',
+			multiplicity: 1,
+			id_parent: '1'
+		},
+		{
+			id: '1.2.1',
+			name: 'Street',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.2'
+		},
+		{
+			id: '1.2.2',
+			name: 'City',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.2'
+		},
+		{
+			id: '1.2.3',
+			name: 'State',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.2'
+		},
+		{
+			id: '1.2.4',
+			name: 'Postal Code',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.2'
+		},
+		{
+			id: '1.3',
+			name: 'Contact',
+			type: 'struc',
+			multiplicity: 1,
+			id_parent: '1'
+		},
+		{
+			id: '1.3.1',
+			name: 'Email',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.3'
+		},
+		{
+			id: '1.3.2',
+			name: 'Phone',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.3'
+		},
+		{
+			id: '1.4',
+			name: 'Family',
+			type: 'struc',
+			multiplicity: 1,
+			id_parent: '1'
+		},
+		{
+			id: '1.4.1',
+			name: 'Spouse',
+			type: 'struc',
+			multiplicity: 1,
+			id_parent: '1.4'
+		},
+		{
+			id: '1.4.1.1',
+			name: 'First Name',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.4.1'
+		},
+		{
+			id: '1.4.1.2',
+			name: 'Last Name',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.4.1'
+		},
+		{
+			id: '1.4.2',
+			name: 'Children',
+			type: 'struc',
+			multiplicity: 1,
+			id_parent: '1.4'
+		},
+		{
+			id: '1.4.2.1',
+			name: 'Child 1',
+			type: 'struc',
+			multiplicity: 1,
+			id_parent: '1.4.2'
+		},
+		{
+			id: '1.4.2.1.1',
+			name: 'First Name',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.4.2.1'
+		},
+		{
+			id: '1.4.2.1.2',
+			name: 'Last Name',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.4.2.1'
+		},
+		{
+			id: '1.4.2.1.3',
+			name: 'Age',
+			type: 'number',
+			multiplicity: 1,
+			id_parent: '1.4.2.1'
+		},
+		{
+			id: '1.4.2.2',
+			name: 'Child 2',
+			type: 'struc',
+			multiplicity: 1,
+			id_parent: '1.4.2'
+		},
+		{
+			id: '1.4.2.2.1',
+			name: 'First Name',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.4.2.2'
+		},
+		{
+			id: '1.4.2.2.2',
+			name: 'Last Name',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.4.2.2'
+		},
+		{
+			id: '1.4.2.2.3',
+			name: 'Age',
+			type: 'number',
+			multiplicity: 1,
+			id_parent: '1.4.2.2'
+		},
+		{
+			id: '1.5',
+			name: 'Occupation',
+			type: 'struc',
+			multiplicity: 1,
+			id_parent: '1'
+		},
+		{
+			id: '1.5.1',
+			name: 'Job Title',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.5'
+		},
+		{
+			id: '1.5.2',
+			name: 'Company',
+			type: 'struc',
+			multiplicity: 1,
+			id_parent: '1.5'
+		},
+		{
+			id: '1.5.2.1',
+			name: 'Company Name',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.5.2'
+		},
+		{
+			id: '1.5.2.2',
+			name: 'Company Address',
+			type: 'struc',
+			multiplicity: 1,
+			id_parent: '1.5.2'
+		},
+		{
+			id: '1.5.2.2.1',
+			name: 'Street',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.5.2.2'
+		},
+		{
+			id: '1.5.2.2.2',
+			name: 'City',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.5.2.2'
+		},
+		{
+			id: '1.5.2.2.3',
+			name: 'State',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.5.2.2'
+		},
+		{
+			id: '1.5.2.2.4',
+			name: 'Postal Code',
+			type: 'string',
+			multiplicity: 1,
+			id_parent: '1.5.2.2'
 		}
 	];
 </script>
 
-{#each elements as element}
-	<RecursiveElement
-		children={element.children}
-		indent={0}
-		structure={elements}
-		name={element.name}
-	/>
-{/each}
+<RecursiveElement indent={0} structure={elements} id={elements[0].id} />
