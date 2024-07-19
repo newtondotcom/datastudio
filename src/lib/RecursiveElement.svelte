a44715dd57fb4ed49f6a042bcc5fc44b66c96036
newtondotcom <asphalt9fr@gmail.com>
2024-07-19 21:48:00 +0200

    feat : set multiplicity to abstract type

--- src/lib/RecursiveElement.svelte
+++ src/lib/RecursiveElement.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import { ChevronDown, ChevronUp } from 'lucide-svelte';
-	import type { Element } from '../ambient'; 
-	import { Input } from "$lib/components/ui/input/index.js";
-    import Delete from './components/studio/Delete.svelte';
-    import Type from './components/studio/Type.svelte';
-    import Multiplicity from './components/studio/Multiplicity.svelte';
-    import Rename from './components/studio/Rename.svelte';
+	import type { Element } from '../ambient';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import Delete from './components/studio/Delete.svelte';
+	import Type from './components/studio/Type.svelte';
+	import Multiplicity from './components/studio/Multiplicity.svelte';
+	import Rename from './components/studio/Rename.svelte';
 
 	export let children: Element[] = [];
 	export let indent = 0;
@@ -20,47 +20,46 @@
 </script>
 
 <div class="w-full px-4">
-	<div class="flex flex-row">
-	<button style="padding-left: {indent}px" 
-	on:click={toggleOpen} 
-	class="bg-gray-400 flex flex-row w-full text-3xl rounded-lg px-4 py-2">
-		{name}
-		{#if open}
-			<ChevronDown size={40} />
-		{:else}
-			<ChevronUp size={40}/>
-		{/if}
-	</button>
-	<Rename />
-	<Delete />
+	<div class="flex w-full flex-row justify-between">
+		<div class="flex flex-row">
+			<button
+				style="padding-left: {indent}px"
+				on:click={toggleOpen}
+				class="flex w-full flex-row rounded-lg px-4 py-2 text-3xl"
+			>
+				{name}
+				{#if open}
+					<ChevronDown size={40} />
+				{:else}
+					<ChevronUp size={40} />
+				{/if}
+			</button>
+			<Multiplicity />
+		</div>
+		<div class="flex flex-row">
+			<Rename />
+			<Delete />
+		</div>
 	</div>
 
 	{#if open}
 		<ul>
 			<div class="overflow-hidden transition-all duration-300 ease-in-out">
 				{#each children as child}
-				<li class="flex flex-row text-2xl py-2 px-2 border-2 rounded-lg mx-1 my-1">
-					{#if child.type == 'struc'}
-					<li>
-					<svelte:self
-						children={child.children}
-						indent={indent + 1}
-						{structure}
-						name={child.name}
-						/>
+					<li class="mx-1 my-1 flex flex-row rounded-lg border-2 px-2 py-2 text-2xl">
+						{#if child.type == 'struc'}
+							<svelte:self
+								children={child.children}
+								indent={indent + 1}
+								{structure}
+								name={child.name}
+							/>
+						{:else}
+							<Input value={child.name} type="email" placeholder="Name" class="max-w-xs" />
+							<Type />
+							<Delete />
+						{/if}
 					</li>
-					{:else}
-
-					<Input 
-					value={child.name} 
-					type="email" 
-					placeholder="Name" 
-					class="max-w-xs" />
-					<Type />
-					<Multiplicity />
-					{/if}
-				</li>
-
 				{/each}
 			</div>
 		</ul>
--- src/lib/components/studio/Multiplicity.svelte
+++ src/lib/components/studio/Multiplicity.svelte
@@ -1,39 +1,18 @@
-<div class="flex flex-row text-lg items-center">
-    <div>Mltp </div>
-    <Select.Root>
-        <Select.Trigger class="w-[80px]">
-          <Select.Value placeholder="Value" />
-        </Select.Trigger>
-        <Select.Content>
-            <Select.Item value="0">0</Select.Item>
-            <Select.Item value="1">1</Select.Item>
-            <Select.Item value="n">n</Select.Item>
-        </Select.Content>
-      </Select.Root>
-    <div>:</div>
-    <Select.Root>
-        <Select.Trigger class="w-[80px]">
-          <Select.Value placeholder="Value" />
-        </Select.Trigger>
-        <Select.Content>
-          <Select.Item value="0">0</Select.Item>
-          <Select.Item value="1">1</Select.Item>
-          <Select.Item value="n">n</Select.Item>
-        </Select.Content>
-      </Select.Root>
-      <div>with</div>
-    <Select.Root>
-        <Select.Trigger class="w-[180px]">
-          <Select.Value placeholder="Field" />
-        </Select.Trigger>
-        <Select.Content>
-          <Select.Item value="light">Light</Select.Item>
-          <Select.Item value="dark">Dark</Select.Item>
-          <Select.Item value="system">System</Select.Item>
-        </Select.Content>
-      </Select.Root>
-</div>
-
 <script lang="ts">
-    import * as Select from "$lib/components/ui/select";
-  </script>
+	import * as Select from '$lib/components/ui/select';
+	import { SquareM } from 'lucide-svelte';
+</script>
+
+<div class="flex flex-row items-center text-lg">
+	<div class="mx-1"><SquareM /></div>
+	<Select.Root>
+		<Select.Trigger class="w-[80px]">
+			<Select.Value placeholder="Value" />
+		</Select.Trigger>
+		<Select.Content>
+			<Select.Item value="0">0</Select.Item>
+			<Select.Item value="1">1</Select.Item>
+			<Select.Item value="n">n</Select.Item>
+		</Select.Content>
+	</Select.Root>
+</div>
