f9126184549ccce2949746f0d1cfe6f6c9bfc98c
newtondotcom <asphalt9fr@gmail.com>
2024-07-19 20:33:31 +0200

    chore : shadcn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
-<script>
-	import '../app.css';
-</script>
+
 
 <div class="w-screen h-screen flex flex-col justify-center align-middle items-center text-center">
-	<slot />
+	<slot></slot>
 </div>
+<slot></slot><slot></slot><script>import "../app.css";</script>
