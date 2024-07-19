d77c479b87ba822130af62dfee7d85669b71b703
newtondotcom <asphalt9fr@gmail.com>
2024-07-19 21:18:53 +0200

    feat : delete button component

---
+++ src/lib/components/studio/Delete.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import * as AlertDialog from "$lib/components/ui/alert-dialog/index.js";
+    import { Button } from "$lib/components/ui/button/index.js";
+    import { Trash2 } from "lucide-svelte";
+  </script>
+   
+  <AlertDialog.Root>
+    <AlertDialog.Trigger asChild let:builder>
+      <Button builders={[builder]}><Trash2 /></Button>
+    </AlertDialog.Trigger>
+    <AlertDialog.Content>
+      <AlertDialog.Header>
+        <AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+        <AlertDialog.Description>
+          This action cannot be undone. This will permanently delete your account
+          and remove your data from our servers.
+        </AlertDialog.Description>
+      </AlertDialog.Header>
+      <AlertDialog.Footer>
+        <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+        <AlertDialog.Action>Continue</AlertDialog.Action>
+      </AlertDialog.Footer>
+    </AlertDialog.Content>
+  </AlertDialog.Root>
