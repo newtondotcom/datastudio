059d56c8db12c0eda3f2274056f91040283ce4bb
newtondotcom <asphalt9fr@gmail.com>
2024-07-22 12:52:44 +0200

    feat : initial values in the 2 stores

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -3,3 +3,31 @@ import { writable } from 'svelte/store';
 export const structure: Element[] = writable([]);
 
 export const types: Types[] = writable([]);
+
+structure.set([
+	{
+		id: 'c8hs80nis',
+		id_parent: null,
+		name: 'name',
+		type: 'type',
+		multiplicity: 0,
+		color: 'orange-950',
+		indent: 0
+	},
+	{
+		id: 'uhhmwwa8o',
+		name: 'edit',
+		type: 'string',
+		multiplicity: 0,
+		id_parent: 'c8hs80nis',
+		color: 'orange-800',
+		indent: 1
+	}
+]);
+
+types.set([
+	{
+		name: 'string',
+		abstract: false
+	}
+]);
