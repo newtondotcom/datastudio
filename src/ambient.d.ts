e07ca4125300e2a1a53848c72527bf2bfaf9a5b3
newtondotcom <asphalt9fr@gmail.com>
2024-07-22 12:52:23 +0200

    feat : more precise types

--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -1,7 +1,23 @@
 export interface Element {
-	id?: string;
+	id: string;
 	name: string;
 	type: string;
 	multiplicity: number;
-	id_parent: string;
+	id_parent?: string;
+	color: string;
+	indent: number;
+}
+
+export interface Type {
+	name: string;
+	abstract: boolean;
+	id_referenced?: string;
+}
+
+export enum Colors {
+	blue,
+	purple,
+	orange,
+	green,
+	rose
 }
