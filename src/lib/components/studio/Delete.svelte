3737aa4439c65fb70bdfd153f4b2cbfdb6b7a4bd
newtondotcom <asphalt9fr@gmail.com>
2024-07-22 17:40:46 +0200

    feat : delete works but with tricks

--- src/lib/components/studio/Delete.svelte
+++ src/lib/components/studio/Delete.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
 	import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
+	import { deleteElement } from '$lib/functions';
 	import { Trash2 } from 'lucide-svelte';
+
+	export let id: string;
+
+	async function del() {
+		await deleteElement(id);
+	}
 </script>
 
 <AlertDialog.Root>
@@ -18,7 +25,7 @@
 		</AlertDialog.Header>
 		<AlertDialog.Footer>
 			<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
-			<AlertDialog.Action>Continue</AlertDialog.Action>
+			<AlertDialog.Action on:click={del}>Continue</AlertDialog.Action>
 		</AlertDialog.Footer>
 	</AlertDialog.Content>
 </AlertDialog.Root>
