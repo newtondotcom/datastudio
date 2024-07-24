162be23daba8b0ba7571ab00795f5ec3f91b0e08
newtondotcom <asphalt9fr@gmail.com>
2024-07-24 14:56:31 +0200

    feat : create types in origin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,27 @@
 <script lang="ts">
 	import RecursiveElement from '$lib/RecursiveElement.svelte';
 	import type { Element } from '../ambient';
-
-	import { createOrigin } from '$lib/functions';
-
+	import { createTypestruct } from '$lib/functions';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
 	import { Plus } from 'lucide-svelte';
 
-	import { structure, types } from '$lib/store';
+	import { structure } from '$lib/store';
 	let structure_local: Element[];
 	let origins: Element[];
 	structure.subscribe((value) => {
 		structure_local = value;
 		origins = value.filter((el: Element) => el.indent === 0);
-		console.log(value);
-	});
-	let types_local: Type[];
-	types.subscribe((value) => {
-		types_local = value;
-		console.log(value);
 	});
 
 	let name: string;
-	let type: string;
 </script>
 
-{#if origins.length > 0}
-	{#each origins as origin}
-		<RecursiveElement indent={0} id={origin.id} />
-	{/each}
-{/if}
 <Dialog.Root>
 	<Dialog.Trigger>
-		<Button class="mt-4">Create a new origin<Plus /></Button>
+		<Button class="mx-1">Create a type<Plus /></Button>
 	</Dialog.Trigger>
 	<Dialog.Content class="sm:max-w-[425px]">
 		<Dialog.Header>
@@ -49,15 +35,17 @@
 				<Label for="name" class="text-right">Name</Label>
 				<Input bind:value={name} id="name" class="col-span-3" />
 			</div>
-			<div class="grid grid-cols-4 items-center gap-4">
-				<Label for="type" class="text-right">Type</Label>
-				<Input bind:value={type} id="type" class="col-span-3" />
-			</div>
 		</div>
 		<Dialog.Footer>
 			<Dialog.Close>
-				<Button type="submit" on:click={createOrigin(name, type)}>Save changes</Button>
+				<Button type="submit" on:click={createTypestruct(name)}>Save changes</Button>
 			</Dialog.Close>
 		</Dialog.Footer>
 	</Dialog.Content>
 </Dialog.Root>
+
+{#if origins.length > 0}
+	{#each origins as origin}
+		<RecursiveElement id={origin.id} />
+	{/each}
+{/if}
