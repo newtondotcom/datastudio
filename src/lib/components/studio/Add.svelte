6375dd25f9ee354eca216dbc51ae97d5d8952382
newtondotcom <asphalt9fr@gmail.com>
2024-07-24 14:59:09 +0200

    feat : add component simplification

--- src/lib/components/studio/Add.svelte
+++ src/lib/components/studio/Add.svelte
@@ -1,23 +1,13 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button/index.js';
-	import { genUID, genColor, addElement } from '$lib/functions';
+	import { createElement } from '$lib/functions';
 	import { Plus } from 'lucide-svelte';
 
 	export let id_parent: string;
-	export let indent_parent: number;
 
 	async function create() {
 		const parentId = await id_parent;
-		const element: Element = {
-			id: await genUID(),
-			name: 'edit',
-			type: 'string',
-			multiplicity: 0,
-			id_parent: parentId,
-			color: await genColor(indent_parent + 1),
-			indent: indent_parent + 1
-		};
-		addElement(element);
+		await createElement(parentId);
 	}
 </script>
 
