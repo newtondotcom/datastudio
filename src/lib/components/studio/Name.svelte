f72157e8d31468e04d4823e277b9fe34bb71b1c8
newtondotcom <asphalt9fr@gmail.com>
2024-07-24 12:23:52 +0200

    feat : component to change the name

---
+++ src/lib/components/studio/Name.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { renameElement } from '$lib/functions';
+	export let id: string;
+
+	let name: string;
+
+	import { structure } from '$lib/store';
+	let structure_local: Element[];
+	structure.subscribe((value: Element[]) => {
+		structure_local = value;
+
+		if (!value.find((el) => el.id === id)) {
+			return;
+		}
+
+		const el = value.find((el) => el.id === id);
+		name = el.name;
+	});
+
+	async function renameEl() {
+		await renameElement(id, name);
+	}
+</script>
+
+<Input bind:value={name} type="email" placeholder="Name" class="max-w-xs" on:input={renameEl} />
