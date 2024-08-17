e775de3c452a7f2e86765b01df89f09d2e7ef2a3
newtondotcom <asphalt9fr@gmail.com>
2024-08-17 16:00:08 -0700

    fix : remove action from sonner

--- src/routes/load/+page.svelte
+++ src/routes/load/+page.svelte
@@ -7,11 +7,7 @@
 	variant="outline"
 	on:click={() =>
 		toast.success('Event has been created', {
-			description: 'Sunday, December 03, 2023 at 9:00 AM',
-			action: {
-				label: 'Undo',
-				onClick: () => console.info('Undo')
-			}
+			description: 'Sunday, December 03, 2023 at 9:00 AM'
 		})}
 >
 	Show Toast
