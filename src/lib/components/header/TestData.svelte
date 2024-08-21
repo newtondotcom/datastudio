82fd8fc0f83b6189b2c41685ba66fad5a2934aa9
newtondotcom <asphalt9fr@gmail.com>
2024-08-20 22:13:25 -0700

    chore : test dataset dedicated component

---
+++ src/lib/components/header/TestData.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import t from '$lib/scripts/locales';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { exportStructure } from '$lib/scripts/functions';
+	import type { IStructure } from '../../../ambient';
+	import { exportTestData } from '$lib/scripts/exports/test_data';
+
+	async function exportation() {
+		const structure: IStructure = await exportStructure();
+		const md: string = await exportTestData(structure);
+		console.log(md);
+	}
+</script>
+
+<Button on:click={exportation}>{t('header_test')}</Button>
