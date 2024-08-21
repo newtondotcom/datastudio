083ab4a4c106c41642b55f28304002826aaeeb8f
newtondotcom <asphalt9fr@gmail.com>
2024-08-20 22:12:41 -0700

    chore : hql dedicated component

---
+++ src/lib/components/header/Hql.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { IStructure } from '../../../ambient';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { exportStructure } from '$lib/scripts/functions';
+	import t from '$lib/scripts/locales';
+	import { exportHql } from '$lib/scripts/exports/hql';
+
+	async function exportation() {
+		const structure: IStructure = await exportStructure();
+		const md: string = await exportHql(structure);
+		console.log(md);
+	}
+</script>
+
+<Button on:click={exportation}>{t('header_hql')}</Button>
