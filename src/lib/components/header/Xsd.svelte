cad3c576493331c9d1057adb4a86234f6dfa3ad8
newtondotcom <asphalt9fr@gmail.com>
2024-08-20 22:13:38 -0700

    chore : xsd dedicated component

---
+++ src/lib/components/header/Xsd.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { IStructure } from '../../../ambient';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { exportStructure } from '$lib/scripts/functions';
+	import t from '$lib/scripts/locales';
+	import { exportXsd } from '$lib/scripts/exports/xsd';
+
+	async function exportation() {
+		const structure: IStructure = await exportStructure();
+		const md: string = await exportXsd(structure);
+		console.log(md);
+	}
+</script>
+
+<Button on:click={exportation}>{t('header_xsd')}</Button>
---
+++ src/lib/scripts/exports/xsd.ts
@@ -0,0 +1,33 @@
+import type { IElement, IStructure, IType } from '../../../ambient';
+import { capitalizeFirstLetter } from '../functions';
+
+export async function exportXsd(structure: IStructure) {
+	let markdownString: string = '';
+
+	const elements: IElement[] = structure.elements;
+	const types: IType[] = structure.types;
+
+	const types_struct = types.filter((type: IType) => type.struct);
+
+	// Generate Markdown Tables
+	types_struct.forEach((type: IType) => {
+		const parent: IElement = elements.find(
+			(el: IElement) => el.id_parent === null && el.type == type.name
+		);
+		const children: IElement[] = elements.filter((el: IElement) => el.id_parent === parent.id);
+
+		let localString = `### ${capitalizeFirstLetter(parent.type)}\n`;
+		localString += `| Name         | Type       | Multiplicity | Description |\n`;
+		localString += `|--------------|------------|--------------|-------------|\n`;
+
+		children.map((child: IElement) => {
+			let multiplicity = child.multiplicity ? child.multiplicity.toString() : '';
+			localString += `| ${child.name} | ${capitalizeFirstLetter(child.type)} | ${multiplicity} | ${child.description || ''} |\n`;
+		});
+
+		localString += `\n`;
+		markdownString += localString;
+	});
+
+	return markdownString;
+}
