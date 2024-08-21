d8cabab48254747c4bd029ec831cfd4dd0094299
newtondotcom <asphalt9fr@gmail.com>
2024-08-20 22:13:10 -0700

    chore : pyspark dedicated component

---
+++ src/lib/components/header/PySpark.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { IStructure } from '../../../ambient';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { exportStructure } from '$lib/scripts/functions';
+	import t from '$lib/scripts/locales';
+	import { exportPySpark } from '$lib/scripts/exports/pyspark';
+
+	async function exportation() {
+		const structure: IStructure = await exportStructure();
+		const md: string = await exportPySpark(structure);
+		console.log(md);
+	}
+</script>
+
+<Button on:click={exportation}>{t('header_pyspark')}</Button>
