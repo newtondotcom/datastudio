42523f72a330430b59b3c3591c72d090bbfd73b4
newtondotcom <asphalt9fr@gmail.com>
2024-09-01 11:57:26 +0200

    fix : keystrokes listener using tinykeys

--- src/lib/components/header/Save.svelte
+++ src/lib/components/header/Save.svelte
@@ -2,15 +2,12 @@
 	import { Button } from '$lib/components/ui/button/index.js';
 	import { exportStructure } from '$lib/scripts/functions';
 	import t from '$lib/scripts/locales';
-
-	let is_ctrl_down = false;
-	let is_s_down = false;
+	import { onMount } from 'svelte';
+	import { tinykeys } from 'tinykeys';
 
 	async function on_bind() {
 		console.log('save');
 		await saveJSONFile();
-		//const structure = await exportStructure();
-		//console.log(structure)
 	}
 
 	async function saveJSONFile() {
@@ -28,48 +25,14 @@
 		URL.revokeObjectURL(url);
 	}
 
-	function on_key_down(event: KeyboardEvent) {
-		if (event.repeat) return;
-
-		switch (event.key) {
-			case 'Control':
-				is_ctrl_down = true;
+	onMount(() => {
+		tinykeys(window, {
+			'$mod+s': (event: { preventDefault: () => void; key: any }) => {
 				event.preventDefault();
-				break;
-
-			case 'Meta':
-				is_ctrl_down = true;
-				event.preventDefault();
-				break;
-
-			case 's':
-				is_s_down = true;
-				break;
-		}
-
-		if (is_ctrl_down && is_s_down) {
-			on_bind();
-		}
-	}
-
-	function on_key_up(event: KeyboardEvent) {
-		switch (event.key) {
-			case 'Control':
-				is_ctrl_down = false;
-				event.preventDefault();
-				break;
-
-			case 'Meta':
-				is_ctrl_down = false;
-				event.preventDefault();
-				break;
-
-			case 's':
-				is_s_down = false;
-				break;
-		}
-	}
+				on_bind();
+			}
+		});
+	});
 </script>
 
 <Button on:click={on_bind}>{t('header_save')}</Button>
-<svelte:window on:keydown={on_key_down} on:keyup={on_key_up} />
